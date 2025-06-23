687a46a1af8fd7081dba24f5c016fe5a7cc184b1
Joan <joan1998bocairent@gmail.com>
2025-06-24 00:40:17 +0200

    added to admin area

--- src/Components/organisms/DashboardAdminContent/DashboardAdminContent.jsx
+++ src/Components/organisms/DashboardAdminContent/DashboardAdminContent.jsx
@@ -3,6 +3,9 @@ import { useState } from 'react'
 import { DashboardNav } from '../DashboardNav/DashboardNav'
 import { DashboardAdminMain } from '../DashboardAdminMain/DashboardAdminMain'
 
+
+
+
 import "./DashboardAdminContent.scss"
 
 export const DashboardAdminContent = () => {
--- src/Components/pages/Userprofile/Userprofile.jsx
+++ src/Components/pages/Userprofile/Userprofile.jsx
@@ -3,9 +3,6 @@ import { useEffect, useState } from 'react'
 import { DashboardHeader } from '../../organisms/DashboardHeader/DashboardHeader.jsx'
 import { DashboardNav } from '../../organisms/DashboardNav/DashboardNav.jsx'
 
-
-
-import { DashboargetEmplooyeedsbyProjects } from '../../molecules/DashboardFetch/DashdoargetemployeesdbyProjects/DashboargetEmplooyeedsbyProjects.jsx';
 import { DashboardbyEmplooyeedbyProjets } from '../../molecules/DashboardFetch/DashboardbyEmployeedProjects/DashboardbyEmployeedProjects.jsx';
 
 
@@ -20,8 +17,6 @@ const Userprofile = () => {
             <main className="dashboardContent">
                 <DashboardNav />
                 <section id="main_userprofile">
-                    { /*Admin*/}
-                    { false && < DashboargetEmplooyeedsbyProjects />}
                     { /*User*/}
                     { true && < DashboardbyEmplooyeedbyProjets/> }
                 </section>
