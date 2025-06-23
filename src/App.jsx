f11e07a7542d22a5cf43331e7af77820996c3e86
JNBarrachina <jnb.pasadobinario@gmail.com>
2025-06-23 13:09:27 +0200

    Desarrollando el context, datos del perfil del empleado guardados

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,31 @@ import { Login } from "./Components/Login/Login"
 import { Dashboard } from './Components/pages/Dashboard/Dashboard'
 import { NotFound } from './Components/pages/NotFound/NotFound'
 
+import { UserDataContext } from './contexts/UserDataContext'
+
+
+
 function App() {
+
+  const [userData, setUserData] = useState(() => {
+    const stored = localStorage.getItem("login");
+    return stored ? JSON.parse(stored) : {};
+  });
 
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <UserDataContext.Provider
+        value={{ userData, setUserData }}
+      >
+
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </UserDataContext.Provider>
     </>
   )
 }
