097b777b845c952e1e64c908b0a486dbe119f83c
Vice079 <v_llorens@hotmail.com>
2025-06-24 19:06:53 +0200

    Ordenando entries en sus days

--- src/Components/molecules/DailyWorkEntry/DailyWorkEntry.jsx
+++ src/Components/molecules/DailyWorkEntry/DailyWorkEntry.jsx
@@ -2,13 +2,13 @@ import { useState, useEffect } from "react";
 
 import { RemoveWorkEntryBtn } from "../../atoms/RemoveWorkEntryBtn/RemoveWorkEntryBtn"
 import "./DailyWorkEntry.scss"
-const DailyWorkEntry = () => {
-
+const DailyWorkEntry = ({entry}) => {
+    console.log(entry);
 
     return (
         <article className="dailyWorkEntryCard">
-            <h3 className="entryName">Nombre del subproyecto</h3>
-            <p className="entryTime">Tiempo</p>
+            <h3 className="entryName">{entry.Subproject.name}</h3>
+            <p className="entryTime">{entry.hours}H</p>
             <RemoveWorkEntryBtn action={() => { }} />
         </article>
     )
