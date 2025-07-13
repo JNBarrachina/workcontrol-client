ed94a94a6ff488c99e0095c1633e1a2e00e88bcd
JNBarrachina <jnb.pasadobinario@gmail.com>
2025-07-13 15:49:58 +0200

    Fix de los estilos del layout

--- src/components/organisms/DashboardAdminMain/DashboardAdminMain.jsx
+++ src/components/organisms/DashboardAdminMain/DashboardAdminMain.jsx
@@ -4,7 +4,7 @@ import "./DashboardAdminMain.scss"
 
 export const DashboardAdminMain = ({ mainContent }) => {
     return (
-        <article className="dashboardAdminMainContent" style={{ marginTop: '4.05rem', height: '91%' }}>
+        <article className="dashboardAdminMainContent">
             {true && mainContent}
         </article>
     )
