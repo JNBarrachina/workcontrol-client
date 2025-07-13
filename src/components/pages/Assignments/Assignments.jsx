7b27b311af4933897647f415255396229caab621
Joan <joan1998bocairent@gmail.com>
2025-07-14 00:44:37 +0200

    eliminando estilos en linea I, refactorizando, mejores estilos en profile

--- src/components/pages/Assignments/Assignments.jsx
+++ src/components/pages/Assignments/Assignments.jsx
@@ -1,11 +1,5 @@
-import { useEffect, useState } from 'react'
-
-import { DashboardHeader } from '../../organisms/DashboardHeader/DashboardHeader.jsx'
-import { DashboardNav } from '../../organisms/DashboardNav/DashboardNav.jsx'
-
 import { DashboardbyEmplooyeedbyProjets } from '../../molecules/DashboardFetch/DashboardbyEmployeedProjects/DashboardbyEmployeedProjects.jsx';
 
-import '../../organisms/DashboardContent/DashboardContent.scss';
 const Assignments = () => {
     return (
             < DashboardbyEmplooyeedbyProjets />
