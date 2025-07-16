30daeb4be59520ff78d91c654201da5716e872b1
JNBarrachina <jnb.pasadobinario@gmail.com>
2025-07-16 10:34:25 +0200

    Fix h3 inside p

--- src/components/molecules/GraphSummary/GraphSummary.jsx
+++ src/components/molecules/GraphSummary/GraphSummary.jsx
@@ -1,7 +1,7 @@
 import { useState, useContext, useEffect } from 'react';
 import { MonthlyEntriesContext } from '../../../contexts/MonthlyEntriesContext';
 import { ProjectsManagerContext } from "../../../contexts/ProjectsManagerContext";
-import {SimplePieChart} from './SimplePieChart';
+import { SimplePieChart } from './SimplePieChart';
 
 import './GraphSummary.scss';
 
@@ -9,45 +9,45 @@ export const GraphSummary = ({ isOpen, onClose, date }) => {
 
     const { entries } = useContext(MonthlyEntriesContext);
     const { projectsManager } = useContext(ProjectsManagerContext);
-    const [worksaux, setWorksaux] = useState (null)
+    const [worksaux, setWorksaux] = useState(null)
     const [pieData, setPieData] = useState(null);
-    const [european, setEuropean] = useState (null)
-    const [noteuropean, setNoteuropean] = useState (null)
+    const [european, setEuropean] = useState(null)
+    const [noteuropean, setNoteuropean] = useState(null)
 
     const [getboolsignature, setboolsignature] = useState(false);
     const [getsignature, setsignature] = useState();
 
     useEffect(() => {
-    const works = entries.map(element => ({
-        project: element.Subproject?.Project.name,
-        subproject: element.Subproject?.name || "",
-        hours: element.hours || 0,
-        isEuropean: element.Subproject?.isEuropean || false
-    }));
-    works.forEach(element => {
-        const match = projectsManager.find(p => p.name === element.project);
-        if (match) element.isEuropean = match.isEuropean;
-    });
-    const rep = [0, 0];
-    works.forEach(work => {
-        work.isEuropean
-            ? (rep[0] += Number(work.hours))
-            : (rep[1] += Number(work.hours));
-    });
-    setWorksaux (works)
-    setEuropean (works.filter(work => work.isEuropean))
-    setNoteuropean (works.filter(work => !work.isEuropean))
-    setPieData([
-        { name: "Europeo", value: rep[0] },
-        { name: "No Europeo", value: rep[1] }
-    ]);
+        const works = entries.map(element => ({
+            project: element.Subproject?.Project.name,
+            subproject: element.Subproject?.name || "",
+            hours: element.hours || 0,
+            isEuropean: element.Subproject?.isEuropean || false
+        }));
+        works.forEach(element => {
+            const match = projectsManager.find(p => p.name === element.project);
+            if (match) element.isEuropean = match.isEuropean;
+        });
+        const rep = [0, 0];
+        works.forEach(work => {
+            work.isEuropean
+                ? (rep[0] += Number(work.hours))
+                : (rep[1] += Number(work.hours));
+        });
+        setWorksaux(works)
+        setEuropean(works.filter(work => work.isEuropean))
+        setNoteuropean(works.filter(work => !work.isEuropean))
+        setPieData([
+            { name: "Europeo", value: rep[0] },
+            { name: "No Europeo", value: rep[1] }
+        ]);
 
-}, [entries]);
+    }, [entries]);
 
     const formatDateHeader = (date) => {
         const [year, month] = date.split("-");
         const monthName = new Date(date + "-01").toLocaleString("en-US", { month: "long" });
-            return `Timesheet of ${monthName} ${year}`;
+        return `Timesheet of ${monthName} ${year}`;
     };
 
 
@@ -96,15 +96,15 @@ export const GraphSummary = ({ isOpen, onClose, date }) => {
     const cargarFirmados = async () => {
         try {
 
-        const nombre = JSON.parse(localStorage.getItem('login'))?.name;
-        const email = JSON.parse(localStorage.getItem('login'))?.email;
+            const nombre = JSON.parse(localStorage.getItem('login'))?.name;
+            const email = JSON.parse(localStorage.getItem('login'))?.email;
 
             const response = await fetch(
                 `http://localhost:3000/uploads/firma/${email}/${nombre}`
             );
 
             console.log(response);
-            
+
             if (!response.ok) {
                 throw new Error(`Error ${response.status}: ${response.statusText}`);
             }
@@ -117,7 +117,7 @@ export const GraphSummary = ({ isOpen, onClose, date }) => {
             const imageUrl = URL.createObjectURL(blob);
             setsignature(imageUrl);
             setboolsignature(true);
-            
+
         } catch (err) {
             console.error("Error al obtener la firma:", err);
             setsignature(null);
@@ -127,87 +127,87 @@ export const GraphSummary = ({ isOpen, onClose, date }) => {
 
     return (
         <>
-        { pieData && (
-            <div className={`sideGraphModal ${isOpen ? 'open' : ''}`}>
-                <div className="modalHeader">
-                    <h3>{formatDateHeader(date)}</h3>
-                    <button className="closeBtn" onClick={onClose}>✕</button>
-                </div>
-                <div className="modalContent">
-                {pieData && (
-                    <div style={{ margin: '2em auto', width: 'fit-content' }}>
-                        <SimplePieChart data={pieData} />
+            {pieData && (
+                <div className={`sideGraphModal ${isOpen ? 'open' : ''}`}>
+                    <div className="modalHeader">
+                        <h3>{formatDateHeader(date)}</h3>
+                        <button className="closeBtn" onClick={onClose}>✕</button>
                     </div>
-                )}
-                <p><h3>European Projects:</h3></p>
-                {summarizeEntriesByProject(european || []).map(project => (
-                    <div key={project.projectName} className="projectSummary">
-                        <h4>{project.projectName} – {project.totalHours.toFixed(2)} h</h4>
-                        <ul>
-                            {project.subprojects.map(sub => (
-                                <li key={sub.name}>
-                                    {sub.name}: {sub.hours.toFixed(2)} h
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                ))}
-                {european.length === 0 && (
-                    <p className="noData">No work entries this month</p>
-                )}
-                <p><h3>Not european Projects:</h3></p>
-                {summarizeEntriesByProject(noteuropean || []).map(project => (
-                    <div key={project.projectName} className="projectSummary">
-                        <h4>{project.projectName} – {project.totalHours.toFixed(2)} h</h4>
-                        <ul>
-                            {project.subprojects.map(sub => (
-                                <li key={sub.name}>
-                                    {sub.name}: {sub.hours.toFixed(2)} h
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                ))}
-                {noteuropean.length === 0 && (
-                    <p className="noData">No work entries this month</p>
-                )}
-                {
-                    true
-                    &&
-                    <>
+                    <div className="modalContent">
+                        {pieData && (
+                            <div style={{ margin: '2em auto', width: 'fit-content' }}>
+                                <SimplePieChart data={pieData} />
+                            </div>
+                        )}
+                        <h3>European Projects:</h3>
+                        {summarizeEntriesByProject(european || []).map(project => (
+                            <div key={project.projectName} className="projectSummary">
+                                <h4>{project.projectName} – {project.totalHours.toFixed(2)} h</h4>
+                                <ul>
+                                    {project.subprojects.map(sub => (
+                                        <li key={sub.name}>
+                                            {sub.name}: {sub.hours.toFixed(2)} h
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                        ))}
+                        {european.length === 0 && (
+                            <p className="noData">No work entries this month</p>
+                        )}
+                        <h3>Not european Projects:</h3>
+                        {summarizeEntriesByProject(noteuropean || []).map(project => (
+                            <div key={project.projectName} className="projectSummary">
+                                <h4>{project.projectName} – {project.totalHours.toFixed(2)} h</h4>
+                                <ul>
+                                    {project.subprojects.map(sub => (
+                                        <li key={sub.name}>
+                                            {sub.name}: {sub.hours.toFixed(2)} h
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                        ))}
+                        {noteuropean.length === 0 && (
+                            <p className="noData">No work entries this month</p>
+                        )}
+                        {
+                            true
+                            &&
+                            <>
 
-                    {
-                        (!getboolsignature) && <>
-                            <section style={{width:'100%',display:'flex', justifyContent:"center"}}>
-                                <button onClick={ cargarFirmados }> Assign Signature </button>
-                            </section>
-                        </>
-                    }
-                    {
-                        getboolsignature && <>
-                            <section style={{
-                                width:'100%',
-                                display:'flex',
-                                flexDirection:'column',
-                                justifyContent:"center",
-                                alignItems:'center',
-                                padding:'5px'
-                                }}>
-                                <img src={getsignature} alt="signature" style={{
-                                    background:'#fff',
-                                    border:'1px solid #000',
-                                    width:'90%',
-                                    borderRadius:'12px',
-                                    }}/>
-                            </section>
-                        </>
-                    }
-                    </>
-                }
+                                {
+                                    (!getboolsignature) && <>
+                                        <section style={{ width: '100%', display: 'flex', justifyContent: "center" }}>
+                                            <button onClick={cargarFirmados}> Assign Signature </button>
+                                        </section>
+                                    </>
+                                }
+                                {
+                                    getboolsignature && <>
+                                        <section style={{
+                                            width: '100%',
+                                            display: 'flex',
+                                            flexDirection: 'column',
+                                            justifyContent: "center",
+                                            alignItems: 'center',
+                                            padding: '5px'
+                                        }}>
+                                            <img src={getsignature} alt="signature" style={{
+                                                background: '#fff',
+                                                border: '1px solid #000',
+                                                width: '90%',
+                                                borderRadius: '12px',
+                                            }} />
+                                        </section>
+                                    </>
+                                }
+                            </>
+                        }
 
+                    </div>
                 </div>
-            </div>
-        )}
+            )}
         </>
     );
 };
